7884754b0d206774b4960a08a1039778d16b5e8b
Frank Nguyen <frank.nguyen@solveware.co>
2021-07-09 21:31:40 +0800

    :art: Run Prettier

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+import express from "express";
 
 export const app = express();
 const port = 5000;
 
-app.get('/api', (req, res) => {
-    res.send('Hello World');
+app.get("/api", (req, res) => {
+  res.send("Hello World");
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
--- src/tests/helpers/reporter.ts
+++ src/tests/helpers/reporter.ts
@@ -1,18 +1,15 @@
 import {
-    DisplayProcessor,
-    SpecReporter,
-    StacktraceOption
-} from 'jasmine-spec-reporter';
+  DisplayProcessor,
+  SpecReporter,
+  StacktraceOption,
+} from "jasmine-spec-reporter";
 import CustomReporter = jasmine.CustomReporter;
 import JasmineStartedInfo = jasmine.JasmineStartedInfo;
 
 class CustomProcessor extends DisplayProcessor {
-    public displayJasmineStarted(
-        info: JasmineStartedInfo,
-        log: string
-    ): string {
-        return `TypeScript ${log}`;
-    }
+  public displayJasmineStarted(info: JasmineStartedInfo, log: string): string {
+    return `TypeScript ${log}`;
+  }
 }
 
 jasmine.getEnv().clearReporters();
