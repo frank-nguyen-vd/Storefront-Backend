ca2390ef5f3a89d30b89e9f7b8c8006be2b47e04
Frank Nguyen <frank.nguyen@solveware.co>
2021-07-09 21:35:36 +0800

    :art: Fix Eslint errors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-import express from "express";
+import express from 'express';
 
-export const app = express();
+const app = express();
+export default app;
+
 const port = 5000;
 
-app.get("/api", (req, res) => {
-  res.send("Hello World");
+app.get('/api', (req, res) => {
+  res.send('Hello World');
 });
 
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Listening on port ${port}!`));
