3de19a958bf78fb0b4bbdf18d4ca6a343a31242d
Frank Nguyen <frank.nguyen@solveware.co>
2021-07-09 22:40:18 +0800

    :wrench: Configure Swagger

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 
 const app = express();
 export default app;
 
 const port = 5000;
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/api', (req, res) => {
   res.send('Hello World');
