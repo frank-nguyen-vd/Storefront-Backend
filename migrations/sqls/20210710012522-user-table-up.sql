8018d2c1ff6b33cf13f1b09fca3c33d7b50381c7
Frank Nguyen <frank.nguyen@solveware.co>
2021-07-11 11:14:11 +0800

    :wrench: Add username to table users

--- migrations/sqls/20210710012522-user-table-up.sql
+++ migrations/sqls/20210710012522-user-table-up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
+    first_name VARCHAR(250),
+    last_name VARCHAR(250),
+    username VARCHAR(250),
     password VARCHAR(250)
 );
