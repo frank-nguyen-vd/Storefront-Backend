64a6b66be46c08eab3e49c6a03808ddd627229db
Frank Nguyen <frank.nguyen@solveware.co>
2021-07-11 10:58:20 +0800

    :sparkles: Handle POST /api/products

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,20 +1,42 @@
 import express from 'express';
 import { Product } from '../models/product.model';
+import {
+  createErrMsg,
+  createSuccessMsg,
+} from '../services/response-template.service';
 
-const productsController = express.Router();
+const router = express.Router();
 
-productsController.get(
-  '/',
-  async (
-    req: express.Request,
-    res: express.Response,
-  ): Promise<express.Response | undefined> => {
-    const results = await Product.index();
-    return res.status(200).json({
-      statusCode: 200,
-      data: results,
-    });
-  },
-);
+router.get('/', async (req: express.Request, res: express.Response): Promise<
+  express.Response | undefined
+> => {
+  const results = await Product.index();
+  return res.status(200).json({
+    statusCode: 200,
+    data: results,
+  });
+});
 
-export default productsController;
+router.post('/', async (req: express.Request, res: express.Response): Promise<
+  express.Response | undefined
+> => {
+  if (req.body === {}) {
+    return res
+      .status(400)
+      .send(createErrMsg(400, 'Missing information of the new product'));
+  }
+  const { name, price, category } = req.body;
+
+  if (!name)
+    return res.status(400).send(createErrMsg(400, 'Missing product name'));
+  if (!price)
+    return res.status(400).send(createErrMsg(400, 'Missing product price'));
+  if (!category)
+    return res.status(400).send(createErrMsg(400, 'Missing product category'));
+
+  const result = await Product.create({ name, price, category });
+
+  return res.status(200).send(createSuccessMsg(200, result));
+});
+
+export default router;
