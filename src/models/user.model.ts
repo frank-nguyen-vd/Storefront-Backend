70b49aaff38b8adab40f854f195ba6f4cfac0b35
Frank Nguyen <frank.nguyen@solveware.co>
2021-07-11 19:02:22 +0800

    :sparkles: Add `User.find()`

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -39,4 +39,24 @@ export class User {
       throw new Error(`Cannot create user ${err}`);
     }
   }
+
+  static async find(data: UserType): Promise<UserType[]> {
+    try {
+      const conn = await Client.connect();
+      const { id, first_name, last_name, username } = data;
+      let conditions = '';
+      if (id) conditions += `id = ${id}`;
+      if (first_name) conditions += `first_name = '${first_name}'`;
+      if (last_name) conditions += `last_name = '${last_name}'`;
+      if (username) conditions += `username = '${username}'`;
+      const sql = `SELECT * FROM users WHERE ${conditions}`;
+
+      const result = await conn.query(sql);
+      conn.release();
+
+      return result.rows;
+    } catch (err) {
+      throw new Error(`Cannot find user ${err}`);
+    }
+  }
 }
