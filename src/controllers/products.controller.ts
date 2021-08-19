df2fdd1021b88f8c5174f3284b39e3948202ddf2
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-08-19 11:28:28 +0800

    :art: Prettify products.controller

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -7,36 +7,46 @@ import {
 
 const router = express.Router();
 
-router.get('/', async (req: express.Request, res: express.Response): Promise<
-  express.Response | undefined
-> => {
-  const results = await Product.index();
-  return res.status(200).json({
-    statusCode: 200,
-    data: results,
-  });
-});
+router.get(
+  '/',
+  async (
+    req: express.Request,
+    res: express.Response,
+  ): Promise<express.Response | undefined> => {
+    const results = await Product.index();
+    return res.status(200).json({
+      statusCode: 200,
+      data: results,
+    });
+  },
+);
 
-router.post('/', async (req: express.Request, res: express.Response): Promise<
-  express.Response | undefined
-> => {
-  if (req.body === {}) {
-    return res
-      .status(400)
-      .send(createErrMsg(400, 'Missing information of the new product'));
-  }
-  const { name, price, category } = req.body;
+router.post(
+  '/',
+  async (
+    req: express.Request,
+    res: express.Response,
+  ): Promise<express.Response | undefined> => {
+    if (req.body === {}) {
+      return res
+        .status(400)
+        .send(createErrMsg(400, 'Missing information of the new product'));
+    }
+    const { name, price, category } = req.body;
 
-  if (!name)
-    return res.status(400).send(createErrMsg(400, 'Missing product name'));
-  if (!price)
-    return res.status(400).send(createErrMsg(400, 'Missing product price'));
-  if (!category)
-    return res.status(400).send(createErrMsg(400, 'Missing product category'));
+    if (!name)
+      return res.status(400).send(createErrMsg(400, 'Missing product name'));
+    if (!price)
+      return res.status(400).send(createErrMsg(400, 'Missing product price'));
+    if (!category)
+      return res
+        .status(400)
+        .send(createErrMsg(400, 'Missing product category'));
 
-  const result = await Product.create({ name, price, category });
+    const result = await Product.create({ name, price, category });
 
-  return res.status(200).send(createSuccessMsg(200, result));
-});
+    return res.status(200).send(createSuccessMsg(200, result));
+  },
+);
 
 export default router;
